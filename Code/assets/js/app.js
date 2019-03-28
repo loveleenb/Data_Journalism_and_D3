dcbdabd9c639b78660b45853c84164ac410b6720
loveleenb <44782456+loveleenb@users.noreply.github.com>
2019-03-27 20:19:26 -0700

    Update app.js

--- Code/assets/js/app.js
+++ Code/assets/js/app.js
@@ -1,49 +1,133 @@
-// @TODO: YOUR CODE HERE!
-var svgWidth = 1000;
+var svgWidth = 960;
 var svgHeight = 500;
 
-// create an SVG element
-var svg = d3.select(".article")
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-// Load csv data
-d3.csv("data.csv", function(error, jData) {
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  if (error) return console.warn(error);
+// Load csv data 
+// https://cors.io/?
 
-  console.log(jData);
+//url = "https://cors.io/assets/data/data.csv"
+d3.csv("assets/data/data.csv").then(function(jData) {
 
+  //if (error) return console.warn(error);
+
+  //console.log(jData);
+
+  //var healthcare = [];
+  //var poverty = [];
   // cast the data from the csv as numbers
   jData.forEach(function(data) {
     data.healthcare = +data.healthcare;
     data.poverty = +data.poverty;
+    //healthcare.push(+data.healthcare);
+    //poverty.push(+data.poverty);
+
   });
-  
+  //console.log(healthcare);
+  //console.log(poverty)
+
   // Create a scale for your independent (x) coordinates
   var xScale = d3.scaleLinear()
-    .domain(d3.extent(jData, d => d.poverty))
-    .range([0, svgWidth]);
+    .domain([8, d3.max(jData, d => d.poverty)])
+    .range([0, width]);
 
   // Create a scale for your dependent (y) coordinates
   var yScale = d3.scaleLinear()
     .domain([0, d3.max(jData, d => d.healthcare)])
-    .range([svgHeight, 0]);
+    .range([height, 0]);
 
-// create a line generator function and store as a variable
-  // use the scale functions for x and y data
-  var createLine = d3.line()
-  .x(data => xScale(data.poverty))
-  .y(data => yScale(data.healthcare));  
+      // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);  
 
-});
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
 
-// Append a path element to the svg, make sure to set the stroke, stroke-width, and fill attributes.
-svg.append("path")
-.attr("stroke", "black")
-.attr("stroke-width", "1")
-.attr("fill", "none")
-.attr("d", createLine(jData));
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(jData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.poverty))
+    .attr("cy", d => yScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+    
+    var text = svg.append("g")
+      .attr("class", "labels")
+      .selectAll("text")
+      .data(jData)
+      .enter()
+      .append("text")
+
+      .attr("x", d => xScale(d.poverty +1.4))
+      .attr("y", d => yScale(d.healthcare + -1.5))
+	    .attr("font-size", 10)
+	    .attr("text-anchor", "middle")
+	    .attr("fill","black")
+	    .attr("class", "MyText")
+      .text (d=>d.abbr); 
+    
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.state}<br>Healthcare: ${d.healthcare}<br>Poverty: ${d.poverty}`);
+      });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+      // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data, index) {
+        toolTip.hide(data);
+      });
+
+          // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
 
 });
